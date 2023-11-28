d86b2eaae195a0d8b222c43600bed0b05bcdd99e
corinahincu <hincucorina97@gmail.com>
2023-11-28 15:16:24 +0200

    snow flae

---
+++ src/flake/component.js
@@ -0,0 +1,22 @@
+import './style.scss'
+import { randInt, randColor } from '../helpers/generators'
+
+const Flake = ({
+  size = randInt(20,40),
+  color = randColor()
+}) => {
+
+  const style = {
+    '--size': `${size}px`,
+    '--color': color,
+  };
+  return (
+    <div 
+      style={style}
+      className="flake"
+    >
+    </div>
+  )
+}
+
+export default Flake
