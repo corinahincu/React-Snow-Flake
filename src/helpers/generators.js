071e21e7ac3231c8d393c977a8eb2f5ec33d5449
corinahincu <hincucorina97@gmail.com>
2023-12-26 23:27:32 +0200

    new changes

--- src/helpers/generators.js
+++ src/helpers/generators.js
@@ -1,22 +1,106 @@
 // contains helper functions
 
-const randInt = (min=0,max=1) => {
+const randInt = (min = 0, max = 1) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+const randColor = ({ greyish = 0 } = {}) => {
+  const generateComponent = (base, factor) =>
+    Math.floor(base + (128 - base) * (1 - factor));
+
+  const red = generateComponent(randInt(0, 255), greyish);
+  const green = generateComponent(randInt(0, 255), greyish);
+  const blue = generateComponent(randInt(0, 255), greyish);
+  console.log(`rgb(${red},${green},${blue})`);
+  return `rgb(${red},${green},${blue})`;
+};
+
+const randFloat = (value) => {
+  return value * Math.random();
+};
+
+/* const randInt = (min = 0, max = 1) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+const randColor = ({ 
+  whitiesh = 0, 
+  greyish = 0, 
+  blueish = 0
+} = {}) => {
+
+    let red = randInt(0, 255 * (1 - blueish));
+    let green = randInt(0, 255 * (1 - blueish));
+    let blue = randInt(200, 255 * (1 - blueish)); 
+
+    red = Math.floor(red + (128 - red) * (1 - greyish));
+    green = Math.floor(green + (128 - green) * (1 - greyish));
+    blue = Math.floor(blue + (128 - blue) * (1 - greyish));
+
+    return `rgb(${red},${green},${blue})`;
+};
+
+const randFloat = (value) => {
+  return value * Math.random();
+}; */
+
+/* const randInt = (min=0,max=1) => {
   return Math.floor(
     Math.random()*(max -min) + min
   )
 }
 
-const randColor = ({
+const randColor = ({ whitiesh = 1, greyish = 1, blueish = 1 } = {}) => {
+  let white = randInt(0, 255 * whitiesh);
+  let grey = randInt(0, 255 * greyish);
+  let blue = randInt(0, 255 * blueish);
+
+  return `rgb(${white},${grey},${blue})`;
+};
+
+const randFloat = (value) => {
+  return value * Math.random();
+}; */
+
+
+/* const randInt = (min = 0, max = 1) => {
+  return Math.floor(Math.random() * (max - min) + min);
+}; */
+
+/* const randColor = ({
   redish = 1,
-  greenish = 1,
+  greenish = 0,
   blueish = 1,
+  greyish = 0,
 }={}) => {
   let red = randInt(0,255 * redish)
   let green = randInt(0,255 * greenish)
   let blue = randInt(0,255 * blueish)
+  
+
+    red = Math.floor(red + (128 - red) * (1 - greyish));
+    green = Math.floor(green + (128 - green) * (1 - greyish));
+    blue = Math.floor(blue + (128 - blue) * (1 - greyish));
 
   return `rgb(${red},${green},${blue})`
+}; */
+
+/* const randColor = ({ blueish = 1, greyish = 1 } = {}) => {
+  let red = randInt(0 * (1 - greyish));
+  let green = randInt(0 * (1 - greyish));
+  let blue = randInt(0 * greyish, 255 * blueish); // Adjusted range for blue
+
+  return `rgb(${red},${green},${blue})`;
+}; */
+/* const randColor = ({ greyish = 1} = {}) => {
+  const greyValue = randInt(128 * greyish);
+  return `rgb(${greyValue},${greyValue},${greyValue})`;
 };
 
-export {randInt, randColor}
+const randFloat = (value) => {
+  return value * Math.random()
+} */
+
+
+export {randInt, randColor, randFloat}
 
--- src/snow-effect/snow/component.js
+++ src/snow-effect/snow/component.js
@@ -1,29 +1,75 @@
 import './style.scss'
 import Flake from '../flake/component'
-/* import {randColor} from '../../helpers/generators' */
+import {randColor, randInt, randFloat} from '../../helpers/generators'
 import { useEffect,useState } from 'react'
 
 
 const Snow = ({quantity}) => {
-  
-    let color = "rgb(50,50,255)";
-  let [top,setTop] = useState(0)
-  let [flakes, setFlakes] = useState([
-    <Flake key={1} size={20} top={top} color={color} />,
-  ]); 
-  const TOP_LIMIT = 80
+  let color = randColor();
+  let speed = randFloat(1, 3);
+  let windSpeed = randFloat(0.5, 3.0);
+  let rotationSpeed = 0.3;
+  let top = randInt(-80, 35)
+  let left = randInt(150,0);
+  let size = randInt(5, 30);
 
-  useEffect(()=> {
-    setTimeout(()=>{
-      if(top > TOP_LIMIT){
-        /* flakes.pop() */
-        setFlakes([])
-      } else {
-        setTop(top+5)
-        setFlakes([<Flake key={1} size={20} top={top} color={color} />]);
-      }
-    },500)
-  })
+  let [flakes, setFlakes] = useState([
+
+      <Flake key={2} size={size} speed={speed} top={top} left={left} color={color} />,
+      <Flake key={3} size={size} speed={speed} top={top} left={left} color={color} />,
+    ]); 
+    const TOP_LIMIT = 93
+    const FLAKES_COUNT_LIMIT = 100
+
+// HW1: apply the limit to EACH flake - use the filter hint
+// HW2: refactor the initialization logic - add 5 flakes with random left, color, diff keys
+// HW3: use destructuring + optimization to minize the code inside the setter
+// HW*: make it more realistic
+//   - speed
+//   - delay (with start)
+//   - size
+//   - color
+//   - 
+
+  useEffect(() => {
+    setTimeout(() => {
+      setFlakes(
+        [
+          ...flakes,
+          ...new Array(FLAKES_COUNT_LIMIT - flakes.length)
+            .fill()
+            .map(() => (
+              <Flake
+                key={Math.random()}
+                size={randInt(5, 30)}
+                speed={randFloat(1, 3)}
+                top={randInt(-40, 5)}
+                left={randInt(150,0)}
+                color={randColor()}
+              />
+            )),
+        ]
+          /* .filter((flake) => flake.props.top < TOP_LIMIT)
+            .map((flake) => (
+              <Flake
+                {...flake.props}
+              />
+            )) */
+          .filter((flake) => flake.props.top < TOP_LIMIT)
+          .map((flake) => (
+            <Flake
+              key={flake.key}
+              size={flake.props.size}
+              speed={flake.props.speed}
+              top={flake.props.top + 0.5}
+              left={flake.props.left + windSpeed * flake.props.speed}
+              color={flake.props.color}
+              rotation={rotationSpeed * flake.props.speed}
+            />
+          ))
+      );
+    }, 20);
+  });
 
   return(
     flakes
